54591c05fcdecd35e38b9beeeef72610d4cb5e65
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2023-05-22 08:41:24 -0300

    Update teste.js

--- back-end/src/controllers/teste.js
+++ back-end/src/controllers/teste.js
@@ -2,7 +2,7 @@ const { knex } = require('../database')
 
 module.exports = {
   async teste() {
-    const resultado = await knex.raw('SELECT ID_PRODUTO FROM ST_PRODUTO');
-    return resultado[0]
+    const resultado = await knex.raw(`select * from st_produto`);
+    return resultado
   }
 }
